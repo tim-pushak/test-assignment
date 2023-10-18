f6547d40872709ef127d4b3e16969295dba73833
Артем Пушак <63450170+tim-pushak@users.noreply.github.com>
2023-10-19 01:59:05 +0300

    add: header menu completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+
 //burger menu
 const iconMenu = document.querySelector('.menu__icon');
 if(iconMenu) {
-    const menuBody = document.querySelector('./menu__body');
+    const menuBody = document.querySelector('.menu__body');
     iconMenu.addEventListener("click", function(e) {
         document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('.active');
-        menuBody.classList.toggle('.active');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
     })
 }
